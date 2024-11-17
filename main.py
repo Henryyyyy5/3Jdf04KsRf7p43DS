339e1a2794d3ad19f6defc4ba5400d56f965e37b
Henryyyyy5 <87014369+Henryyyyy5@users.noreply.github.com>
2024-11-17 20:41:01 +0700

    dfzv

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from random import randint
 
 global xlength
-xlength = 6
+xlength = 20
 global ylength
-ylength = 6
+ylength = 20
 global minecount
-minecount = 6
+minecount = 10
 global mines
 mines = [[0 for i in range(xlength)] for j in range(ylength)]
 global visible
@@ -14,8 +14,8 @@ visible = [[False for i in range(xlength)] for j in range(ylength)]
 def show():
   string1 = ""
   string2 = ""
-  for i in range(y):
-    for j in range(x):
+  for i in range(ylength):
+    for j in range(xlength):
       if mines[i][j] < 0:
         string1 += "#"
       elif mines[i][j] == 0:
@@ -23,31 +23,32 @@ def show():
       else:
         string1 += str(mines[i][j])
       if visible[i][j]:
-        string2 += "#"
-      else:
         string2 += "_"
+      else:
+        string2 += "#"
     string1 += "\n"
     string2 += "\n"
-  print(string1,"\n",string2)
+  print(string1,"\n",string2,sep="")
   # print(mines)
 
-def nearcell(list, val):
+def nearcell(list, val, x, y):
+  exec(f"{list}[y][x] {val}")
   if y > 0 and x > 0:
-    exec(f"{list}d[y-1][x-1] += val
+    exec(f"{list}[y-1][x-1] {val}")
   if y > 0:
-    mines[y-1][x] += val
+    exec(f"{list}[y-1][x] {val}")
   if y > 0 and x < xlength-1:
-    mines[y-1][x+1] += val
+    exec(f"{list}[y-1][x+1] {val}")
   if x > 0:
-    mines[y][x-1] += val
+    exec(f"{list}[y][x-1] {val}")
   if x < xlength-1:
-    mines[y][x+1] += val
+    exec(f"{list}[y][x+1] {val}")
   if y < ylength-1 and x > 0:
-    mines[y+1][x-1] += val
+    exec(f"{list}[y+1][x-1] {val}")
   if y < ylength-1:
-    mines[y+1][x] += val
+    exec(f"{list}[y+1][x] {val}")
   if y < ylength-1 and x < xlength-1:
-    mines[y+1][x+1] += val
+    exec(f"{list}[y+1][x+1] {val}")
 
 i = minecount
 while i > 0:
@@ -56,10 +57,11 @@ while i > 0:
   if mines[y][x] < 0:
     continue
   mines[y][x] = -99
-  nearcell("mines","+= 1")
+  nearcell("mines","+= 1", x, y)
   i -= 1
-
-show()
 
 xcur = 1
 ycur = 1
+nearcell("visible","= True", xcur, ycur)
+curs = "◫☐①⚙"
+show()
